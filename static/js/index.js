70337d3dc5fd04b22733e183b15397e05142c0d0
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-09-08 19:04:22 +0100

    test

---
+++ static/js/index.js
@@ -0,0 +1,34 @@
+const breadcrum =  document.querySelector(".breadcrum");
+const body = document.querySelector("body");
+breadcrum.onclick = function(){
+    const nav = document.querySelector(".nav-content");
+    const li = document.querySelectorAll(".nav-content li")
+    nav.classList.toggle("show");
+    
+    li.forEach(elem => {
+        elem.classList.toggle("showLI");
+        // elem.classList.toggle("showLIs");
+    });
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.breadcrum')) {
+    var dropdowns = document.querySelector(".nav-content");
+    if (dropdowns.classList.contains('show')) {
+        dropdowns.classList.remove('show');
+      }
+    // console.log(dropdowns)
+    // var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+  }
+}
+
+
+
